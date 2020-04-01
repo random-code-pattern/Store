f4a175e12753d35ab3850b5a7b489a7126a58ae1
Daniel Batista <danielfilipebatista@gmail.com>
2020-04-01 21:28:09 +0100

    Added controllers, views and models folders

---
+++ src/RandomCodePattern.Store.Web/Controllers/StoreController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RandomCodePattern.Store.Web.Controllers
+{
+    [Route("Store")]
+    public class StoreController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
